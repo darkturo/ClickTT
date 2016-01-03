6579ed531f2a1abd73793d371f9d8ce9868b8f6c
Arturo Escudero <darkturo@gmail.com>
2016-01-03 03:06:42 +0100

    Add a first working version of a script to migrate TimeOrg times to Toggl
    I have pending to include a sample sqllite3 database, containing the
    structure and in which the TimeOrg database comes with.

---
+++ ImportTools/TimeOrg/TimeOrg2Toggl.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import sqlite3
+import argparse
+import sys
+import csv
+
+# Some Specific constants
+START_DATE = 'Start date'
+START_TIME = 'Start time'
+DURATION= 'Duration'
+
+def parseArgs(argv):
+    parser = argparse.ArgumentParser(
+                description = """Simple script to migrate data from an the
+                                 backup with all the time reports tracked by
+                                 TimeOrg to a csv format that can be imported
+                                 into Toggl.""" )
+    parser.add_argument('-f', '--backup-file', action='store', type=str,
+                        required=True, dest='backup', 
+                        help="File conatining the backup with the TimeOrg data")
+    return parser.parse_args( argv )
+
+def migrateTimeOrgTimesToCSV(backup_file):
+    sqliteQueryResult = extractTimesFromBackup( backup_file )
+
+    field_names = [START_DATE, START_TIME, DURATION]
+    togglCSVEmitter = csv.DictWriter(sys.stdout, field_names)
+    togglCSVEmitter.writeheader()
+    for (sdate, stime, duration) in sqliteQueryResult:
+        togglCSVEmitter.writerow({ START_DATE: sdate,
+                                   START_TIME: stime,
+                                   DURATION: duration})
+
+def extractTimesFromBackup(backup_file):
+    conn = sqlite3.connect( backup_file )
+    c = conn.cursor()
+
+    # This query will extract from the LoginTime and LogoutTime (which
+    # represents when you start and stop tracking time with TimeOrg), and then
+    # it output a three colum result table, the first colum with contain the 
+    # start date, the second column the start time, and the third one wil 
+    # contain the duration using the follogin format HH:MM:SS.
+    query = """select strftime("%Y-%m-%d", LoginTime), 
+                      strftime("%H:%M:%S", LoginTime), 
+                      time(strftime('%s', LogoutTime) - strftime('%s', LoginTime),
+                           'unixepoch') 
+               from times;"""
+
+    # executing the query and returning the array of tuples
+    return c.execute( query ).fetchall()
+
+if __name__ == "__main__":
+    options = parseArgs(sys.argv[1:])
+    
+    migrateTimeOrgTimesToCSV( options.backup )
