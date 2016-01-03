0e9dda303bd00fc4340fa044bf2fac0cfa621b30
Arturo Escudero <darkturo@gmail.com>
2016-01-03 04:14:54 +0100

    Add mandatory option "-u" to associate the tacking to a person
    Also add some other non-mandatory fields, that will be used in toggl.

--- ImportTools/TimeOrg/TimeOrg2Toggl.py
+++ ImportTools/TimeOrg/TimeOrg2Toggl.py
@@ -6,9 +6,15 @@ import sys
 import csv
 
 # Some Specific constants
+EMAIL = 'Email'
+CLIENT = 'Client'
+PROJECT = 'Project'
+DESCRIPTION = 'Description'
 START_DATE = 'Start date'
 START_TIME = 'Start time'
 DURATION= 'Duration'
+TAGS = 'Tags'
+BILLABLE = 'Billable'
 
 def parseArgs(argv):
     parser = argparse.ArgumentParser(
@@ -19,12 +25,29 @@ def parseArgs(argv):
     parser.add_argument('-f', '--backup-file', action='store', type=str,
                         required=True, dest='backup', 
                         help="File conatining the backup with the TimeOrg data")
+    parser.add_argument('-u', '--user-email', action='store', type=str,
+                        required=True, dest='email', 
+                        help="Email address to which the report should be associated to")
     parser.add_argument('-o', '--csv-file', action='store', type=str,
                         required=False, dest='csvFile', 
                         help="Target file, where the script will store the CSV output. If nothing is specified, by default the program will use the default std output.")
+    parser.add_argument('-c', '--client', action='store', type=str,
+                        required=False, dest='client', 
+                        help="Use the specified client name for the global report.")
+    parser.add_argument('-p', '--project', action='store', type=str,
+                        required=False, dest='project', 
+                        help="Use the specified project name for the global report.")
+    parser.add_argument('-t', '--tags', action='store', type=str,
+                        required=False, dest='tags', 
+                        help="Use the specified tags for all the entries.")
+    parser.add_argument('--billable', action='store', type=bool,
+                        required=False, default=True, dest='billable', 
+                        help="Specify whether the entries are billable or not. By default I asume yes.")
+
     return parser.parse_args( argv )
 
-def migrateTimeOrgTimesToCSV(backup_file, csv_file):
+def migrateTimeOrgTimesToCSV(user_email, backup_file, csv_file,
+                             client, project, tags, isBillable):
     sqliteQueryResult = extractTimesFromBackup( backup_file )
 
     csv_fh = sys.stdout
@@ -32,13 +55,25 @@ def migrateTimeOrgTimesToCSV(backup_file, csv_file):
         csv_fh = open(csv_file, 'wb')
 
     with csv_fh as fh:
-        field_names = [START_DATE, START_TIME, DURATION]
+        field_names = [ EMAIL, CLIENT, PROJECT, DESCRIPTION, 
+                        START_DATE, START_TIME, DURATION, 
+                        TAGS, BILLABLE ]
+
+        billable_yes_no = "Y"
+        if not isBillable:
+            billable_yes_no = "N"
+
         togglCSVEmitter = csv.DictWriter(fh, field_names)
         togglCSVEmitter.writeheader()
         for (sdate, stime, duration) in sqliteQueryResult:
-            togglCSVEmitter.writerow({ START_DATE: sdate,
+            togglCSVEmitter.writerow({ EMAIL: user_email,
+                                       CLIENT: client,
+                                       PROJECT: project,
+                                       START_DATE: sdate,
                                        START_TIME: stime,
-                                       DURATION: duration})
+                                       DURATION: duration,
+                                       TAGS: tags,
+                                       BILLABLE: billable_yes_no})
 
 def extractTimesFromBackup(backup_file):
     conn = sqlite3.connect( backup_file )
@@ -61,4 +96,5 @@ def extractTimesFromBackup(backup_file):
 if __name__ == "__main__":
     options = parseArgs(sys.argv[1:])
     
-    migrateTimeOrgTimesToCSV( options.backup, options.csvFile )
+    migrateTimeOrgTimesToCSV( options.email, options.backup, options.csvFile,
+                              options.client, options.project, options.tags, options.billable )
